0e916aae7598eadfb035e8b405c69a9687b9f06e
IasonTsivos <iaswnas1o@gmail.com>
2024-12-25 15:26:44 +0200

    firebase init

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,28 @@ import ExteriorSection from "./components/ExteriorSection";
 import PartnersSection from "./components/PartnersSection";
 import ContactSection from "./components/ContactSection";
 import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLmfY7rBCb5UirlIXkYUUfteynVHNGtk0",
+  authDomain: "visualization-2013c.firebaseapp.com",
+  projectId: "visualization-2013c",
+  storageBucket: "visualization-2013c.firebasestorage.app",
+  messagingSenderId: "1035786817358",
+  appId: "1:1035786817358:web:8f23ab9e338edf6fb2581f",
+  measurementId: "G-GHYVDQPH8W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   const [inView, setInView] = useState({
